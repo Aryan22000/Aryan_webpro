9881f94621c539d1ef1b107a5f4991d5e7119dfc
Aryan22000 <113422176+Aryan22000@users.noreply.github.com>
2023-02-02 23:33:08 +0200

    add contact of staff

--- src/Aryan_Mohammadi_Task/tommi project/staff.php
+++ src/Aryan_Mohammadi_Task/tommi project/staff.php
@@ -1,5 +1,11 @@
 <?php include "header.php" ?>
-<!-- <h1>some content</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sint delectus minus optio doloremque incidunt neque impedit enim ipsum autem libero cum, quas adipisci consectetur vitae dicta non exercitationem itaque.</p> -->
+<img src="./images/Aryan.jpg" alt="Aryan Mohammadi" style="height: 500px; width: 500px;"></div>
+       Aryan Mohammadi<br> Chef Aryan, who is originally from Iran, grew up learning the art of cooking from his family and used this knowledge as a launching pad to fuse creativity 
+       and personality to develop his own innovative recipes.
+       He first discovered his talent of cooking when he was young by his family. 
+       Now, he has more than 17 years of experience in cooking industry as an executive chef. 
+       His passion for food truly emerged. 
+       truly Cooking became part of his life. He is totally talented in designing great plans for middle eastern restaurant setup, 
+       Mediterranean restaurant setup, restaurant franchising services and culinary services. <br>
 
 <?php include "footer.php" ?>
