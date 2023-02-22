f0178e20bd4a7c015b17c1ddcc7d5a56c1d75cf0
Aryan22000 <113422176+Aryan22000@users.noreply.github.com>
2023-02-22 12:56:07 +0200

    Update read.php

--- src/Aryan_Mohammadi_Task/crud1/read.php
+++ src/Aryan_Mohammadi_Task/crud1/read.php
@@ -25,7 +25,7 @@ if ($result->num_rows > 0){
               </tr> ";
               }echo
               "</table>";
-              }else{
+}else{
                      echo "NO Results";
                      }
        $con -> close();
